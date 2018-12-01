94af7902241b532bc2b784cb8f9fa23c23aaec58
Mburg Dr J <jaiclinm@mercersburg.edu>
2018-11-30 22:14:30 -0500

    Improved concatenation of columns, improved high quality start.

--- degrom.py
+++ degrom.py
@@ -1,13 +1,52 @@
 import pandas
-import matplotlib
 
+# I am assuming you've gotta throw a minimum number of pitches before calling it
+# a real start.  This is obviously not how baseball is doing things yet, but
+# maybe it should.
+minPit = 60
+
+# Read in 2014 data
 frame2014 = pandas.read_csv('2014.csv')
+short2014 = pandas.concat([ frame2014['ER'], frame2014['Pit'], frame2014['Inngs'] ], axis=1)
+# Calculate 2014 Real Starts (>= minimum number of pitches)
+short2014['RealS'] = short2014['Pit'] > minPit
+# Calculate 2014 High Quality Starts
+short2014['HQS'] = (short2014['ER'] < 2) & (short2014['RealS'])
 
-short2014 = pandas.DataFrame({
-    'ER': frame2014['ER'],
-    'Pit': frame2014['Pit'],
-})
+# Read in 2015 data
+frame2015 = pandas.read_csv('2015.csv')
+short2015 = pandas.concat([ frame2015['ER'], frame2015['Pit'], frame2015['Inngs'] ], axis=1)
+# Calculate 2015 Real Starts (>= minimum number of pitches)
+short2015['RealS'] = short2015['Pit'] > minPit
+# Calculate 2015 High Quality Starts
+short2015['HQS'] = (short2015['ER'] < 2) & (short2015['RealS'])
 
-short2014['HQS'] = (short2014['ER'] < 2) & (short2014['Pit'] > 60)
+# Read in 2016 data
+frame2016 = pandas.read_csv('2016.csv')
+short2016 = pandas.concat([ frame2016['ER'], frame2016['Pit'], frame2016['Inngs'] ], axis=1)
+# Calculate 2016 Real Starts (>= minimum number of pitches)
+short2016['RealS'] = short2016['Pit'] > minPit
+# Calculate 2016 High Quality Starts
+short2016['HQS'] = (short2016['ER'] < 2) & (short2016['RealS'])
 
-print(short2014)
+# Read in 2017 data
+frame2017 = pandas.read_csv('2017.csv')
+short2017 = pandas.concat([ frame2017['ER'], frame2017['Pit'], frame2017['Inngs'] ], axis=1)
+# Calculate 2017 Real Starts (>= minimum number of pitches)
+short2017['RealS'] = short2017['Pit'] > minPit
+# Calculate 2017 High Quality Starts
+short2017['HQS'] = (short2017['ER'] < 2) & (short2017['RealS'])
+
+# Read in 2018 data
+frame2018 = pandas.read_csv('2018.csv')
+short2018 = pandas.concat([ frame2018['ER'], frame2018['Pit'], frame2018['Inngs'] ], axis=1)
+# Calculate 2018 Real Starts (>= minimum number of pitches)
+short2018['RealS'] = short2018['Pit'] > minPit
+# Calculate 2018 High Quality Starts
+short2018['HQS'] = (short2018['ER'] < 2) & (short2018['RealS'])
+
+shortAll = pandas.concat([short2014, short2015, short2016, short2017, short2018], ignore_index=True)
+
+hqs = len(shortAll['HQS'].nonzero()[0])
+all = len(shortAll['RealS'].nonzero()[0])
+print('High Quality Starts:', hqs, "out of", all, "or", round(hqs * 100 / all), "%" )
