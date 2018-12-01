757d9b8b42b760acf29341ea606a87beb031d02b
Mburg Dr J <jaiclinm@mercersburg.edu>
2018-11-30 21:26:22 -0500

    Read one year, restrict, compute high quality starts

--- degrom.py
+++ degrom.py
@@ -1,2 +1,13 @@
 import pandas
-print(pandas.__version__)
+import matplotlib
+
+frame2014 = pandas.read_csv('2014.csv')
+
+short2014 = pandas.DataFrame({
+    'ER': frame2014['ER'],
+    'Pit': frame2014['Pit'],
+})
+
+short2014['HQS'] = (short2014['ER'] < 2) & (short2014['Pit'] > 60)
+
+print(short2014)
